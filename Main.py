62f377b9f7024a9a475c597ff2470ba7fd18d92b
Medhanie24 <medhanie.belay@stu.ocsb.ca>
2024-09-17 11:14:09 -0400

    Random

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 #Ask the user for at least ten questions
 #Read in each answer and store it in a variable
 #Print out the result
-
-print("Welcome to Mad Libs!")
-print("Give me a name: ")
-name=input()
-print("One day",name,"wrote a Mad Lib for class!")
+input("give me adjective")
+("Impossible")
+("give me a person")
+("Ronaldo")
+("give me a verb")
+("eat")
+("give me a vegetable")
