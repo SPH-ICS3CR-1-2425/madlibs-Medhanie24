4d8592276f3e5abc3f4b748165baaf96d82cf0d2
Medhanie24 <medhanie.belay@stu.ocsb.ca>
2024-11-07 10:25:13 -0500

    ,mkopjh

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 #Ask the user for at least ten questions
 #Read in each answer and store it in a variable
 #Print out the result
-input("give me adjective")
-("Impossible")
-("give me a person")
-("Ronaldo")
-("give me a verb")
-("eat")
-("give me a vegetable")
+print("give me adjective")
+adjective=input()
+print("give me a person")
+person=input()
+print("give me a number")
+number=input()
+print("give me a drink")
+drink=input()
+print("give me a name")
+name=input()
+
+input()
+
+print("One day",person,"had a",drink," and it was",adjective)
